77b99728b6dbb031ed4c0cb724bf22aa70a8f785
Student <example@example.com>
2020-03-02 13:48:30 -0800

    add 8 passing tests

--- ModelTests/LeetSpeak.Tests.cs
+++ ModelTests/LeetSpeak.Tests.cs
@@ -28,7 +28,47 @@ namespace LeetSpeak.Tests
     public void IsMakeLeet_ChangingE_3()
     {
       UserString testUserString = new UserString("Hello!");
-      Assert.AreEqual("H3llo!", testUserString.MakeLeet("Hello!"));
+      Assert.AreEqual("H3ll0!", testUserString.MakeLeet("Hello!"));
+    }
+
+    [TestMethod]
+
+    public void IsMakeLeet_ChangingS_Z()
+    {
+      UserString testUserString = new UserString("Songs!");
+      Assert.AreEqual("S0ngz!", testUserString.MakeLeet("Songs!"));
+    }
+
+    [TestMethod]
+
+    public void IsMakeLeet_ChangingO_0()
+    {
+      UserString testUserString = new UserString("Songs!");
+      Assert.AreEqual("S0ngz!", testUserString.MakeLeet("Songs!"));
+    }
+
+    [TestMethod]
+
+    public void IsMakeLeet_ChangingL_1()
+    {
+      UserString testUserString = new UserString("Leet!");
+      Assert.AreEqual("1337!", testUserString.MakeLeet("Leet!"));
+    }
+
+    [TestMethod]
+
+    public void IsMakeLeet_ChangingT_7()
+    {
+      UserString testUserString = new UserString("Leet!");
+      Assert.AreEqual("1337!", testUserString.MakeLeet("Leet!"));
+    }
+
+    [TestMethod]
+
+    public void IsMakeLeet_Changing1S_S()
+    {
+      UserString testUserString = new UserString("I'm a test sentence!");
+      Assert.AreEqual("I'm a 73z7 s3n73nc3!", testUserString.MakeLeet("I'm a test sentence!"));
     }
   }
 }
